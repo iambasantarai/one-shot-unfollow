0d5280a31c6cf4f438fd2398d3316942f347e864
iambasantarai <iambasantarai@gmail.com>
2024-07-27 22:54:42 +0545

    Use ejs as a templating engine

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import url from 'node:url';
+import path from 'node:path';
 import express from 'express';
 
 import routes from './routes/index.js';
@@ -5,6 +7,10 @@ import routes from './routes/index.js';
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+app.set('views', [path.join(__dirname, 'views')]);
+app.set('view engine', 'ejs');
 app.use(routes);
 
 app.listen(PORT, () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
 router.get('/heartbeat', (req, res) => {
   const hrtime = process.hrtime.bigint();
+
   res.status(200).json({ heartbeat: hrtime.toString() });
 });
 
