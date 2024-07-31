d2959e2d139baa1e2a3e0b963c396a5e22fa2add
iambasantarai <iambasantarai@gmail.com>
2024-07-31 19:47:52 +0545

    Render two factor and unfollow pages

--- src/routes/ig.rotue.js
+++ src/routes/ig.rotue.js
@@ -4,6 +4,14 @@ import { loginHandler, logoutHandler } from '../controllers/ig.controller.js';
 
 const router = Router();
 
+router.get('/two-factor', (req, res) => {
+  res.render('twoFactor');
+});
+
+router.get('/unfollow', (req, res) => {
+  res.render('unfollow');
+});
+
 router.post('/login', loginHandler);
 router.post('/logout', logoutHandler);
 
